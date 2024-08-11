80db1af735e6b1d2d4003dc48c16d9e473cc945f
Şükrü Batuhan Turhan <sbturhan@gmail.com>
2024-08-11 22:37:13 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -263,13 +263,13 @@ app.post('/api/user-cards', authenticateToken, async (req, res) => {
     const userId = req.user.id;
     const { card_id, level, current_cost, current_hourly_earnings } = req.body;
 
-    if (typeof level !== 'number' || level <= 0 || level >= 1000) {
+    if (level <= 0 || level >= 1000) {
       return res.status(400).json({ error: 'Invalid Level' });
     }
-    if (typeof current_cost !== 'number' || current_cost <= 0 || current_cost >= 9999999999999) {
+    if (current_cost <= 0 || current_cost >= 9999999999999) {
       return res.status(400).json({ error: 'Invalid Cost' });
     }
-    if (typeof current_hourly_earnings !== 'number' || current_hourly_earnings <= 0 || current_hourly_earnings >= 9999999999) {
+    if (current_hourly_earnings <= 0 || current_hourly_earnings >= 9999999999) {
       return res.status(400).json({ error: 'Invalid PPH' });
     }
 
@@ -292,16 +292,16 @@ app.put('/api/user-cards/:id', authenticateToken, async (req, res) => {
   const { id } = req.params;
   const { current_cost, current_hourly_earnings, level } = req.body;
 
-  if (typeof level !== 'number' || level <= 0 || level >= 1000) {
+  if (level <= 0 || level >= 1000) {
     return res.status(400).json({ error: 'Invalid Level' });
   }
-  if (typeof current_cost !== 'number' || current_cost <= 0 || current_cost >= 9999999999999) {
+  if (current_cost <= 0 || current_cost >= 9999999999999) {
     return res.status(400).json({ error: 'Invalid Cost' });
   }
-  if (typeof current_hourly_earnings !== 'number' || current_hourly_earnings <= 0 || current_hourly_earnings >= 9999999999) {
+  if (current_hourly_earnings <= 0 || current_hourly_earnings >= 9999999999) {
     return res.status(400).json({ error: 'Invalid PPH' });
   }
-  
+
   try {
     const result = await pool.query(
       'UPDATE user_cards SET current_cost = $1, current_hourly_earnings = $2, level = $3 WHERE card_id = $4 AND user_id = $5 RETURNING *',
