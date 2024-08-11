03544dd46a762d0d4381e71ffd06c3bfd6e10e33
Şükrü Batuhan Turhan <sbturhan@gmail.com>
2024-08-12 01:31:54 +0300

    first commit

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './assets/LandingPage.css';
+import './assets/LandingPage.module.css';
 import hamsterImage from './assets/img/Hamster.webp';
-import logoImage from './assets/img/Logo.png'; // logonuzun yolu
+import logoImage from './assets/img/Logo.webp'; // logonuzun yolu
 
-function LandingPage() {
+function Navbar() {
   const navigate = useNavigate();
-  document.title = "Hamster Kombat Tool";
 
   return (
-    <div className="landing-page">
-      {/* Logo */}
+    <div className="navbar">
       <img src={logoImage} alt="Logo" className="logo" />
-
-      <div className="content">
-        {/* Resim */}
-        <div className="image-container">
-          <img src={hamsterImage} alt="Hamster" />
-        </div>
-
-        {/* Başlık ve Açıklama */}
-        <div className="text-content">
-          <h1 className="title">Hamster Kombat Tool</h1>
-          <p className="description">The ultimate tool for Hamster Kombat enthusiasts. Join now and start your journey!</p>
-        </div>
-      </div>
-
-      {/* Login ve Register Butonları */}
       <div className="header-links">
         <button onClick={() => navigate('/login')} className="link-button">Login</button>
         <button onClick={() => navigate('/register')} className="link-button">Register</button>
@@ -35,4 +18,31 @@ function LandingPage() {
   );
 }
 
-export default LandingPage;
+function LandingPage() {
+  document.title = "Hamster Kombat Tool";
+
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <div className="image-container">
+          <img src={hamsterImage} alt="Hamster" className="main-image" />
+        </div>
+        <div className="text-content">
+          <h1 className="title">Hamster Kombat Tool</h1>
+          <p className="description">The ultimate tool for Hamster Kombat enthusiasts. Join now and start your journey!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
