3f544b9802b1ce1e8243a50b8c7d3dc86dde2f0e
Şükrü Batuhan Turhan <sbturhan@gmail.com>
2024-08-12 04:00:07 +0300

    first commit

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from './ThemeContext';
 import styles from './assets/LandingPage.module.css';
 import hamsterImage from './assets/img/Hamster.webp';
 import logoImage from './assets/img/Logo.webp';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
-function App() {
+function LandingPage() {
   const navigate = useNavigate();
+  const { isDarkMode, toggleTheme } = useTheme();
   
   useEffect(() => {
     document.title = "Hamster Kombat Tool";
     
-    // Sayfa yüklendiğinde animasyon efekti
     const content = document.querySelector(`.${styles.content}`);
     content.style.opacity = '0';
     content.style.transform = 'translateY(20px)';
@@ -23,28 +25,28 @@ function App() {
   }, []);
 
   return (
-    <>
+    <div className={`${styles['landing-page']} ${isDarkMode ? styles.dark : ''}`}>
       <div className={styles.navbar}>
         <img src={logoImage} alt="Logo" className={styles.logo}/>
         <div className={styles['header-links']}>
+          <button onClick={toggleTheme} className={styles['theme-toggle']}>
+            {isDarkMode ? <FaSun /> : <FaMoon />}
+          </button>
           <button onClick={() => navigate('/login')} className={styles['link-button']}>Login</button>
           <button onClick={() => navigate('/register')} className={styles['link-button']}>Register</button>
         </div>
       </div>
-      <div className={styles['landing-page']}>
-        <div className={styles.content}>
-          <div className={styles['image-container']}>
-            <img src={hamsterImage} alt="Hamster" className={styles['main-image']} />
-          </div>
-          <div className={styles['text-content']}>
-            <h1 className={styles.title}>Hamster Kombat Tool</h1>
-
-            <p className={styles.description}>Enter the world of strategic hamster battles. Train, compete, and become the ultimate Hamster Kombat master!</p>
-          </div>
+      <div className={styles.content}>
+        <div className={styles['image-container']}>
+          <img src={hamsterImage} alt="Hamster" className={styles['main-image']} />
+        </div>
+        <div className={styles['text-content']}>
+          <h1 className={styles.title}>Hamster Kombat Tool</h1>
+          <p className={styles.description}>Enter the world of strategic hamster battles. Train, compete, and become the ultimate Hamster Kombat master!</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App;
+export default LandingPage;
