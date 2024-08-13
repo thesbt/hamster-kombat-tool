cc1686240bd3280a0995bcfabcbd81a1e73de439
Şükrü Batuhan Turhan <sbturhan@gmail.com>
2024-08-13 04:55:29 +0300

    first commit

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './assets/Register.css';
-import { useNavigate } from 'react-router-dom';
+import styles from './assets/Register.module.css';
+import { useNavigate, Link } from 'react-router-dom';
 import zxcvbn from 'zxcvbn';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaSun, FaMoon, FaUserPlus, FaEye, FaEyeSlash, FaSignInAlt } from 'react-icons/fa';
+import { useTheme } from './ThemeContext';
+import logoImage from './assets/img/Logo.webp';
 
 function Register() {
   const [username, setUsername] = useState('');
@@ -13,19 +15,53 @@ function Register() {
   const [success, setSuccess] = useState('');
   const [passwordStrength, setPasswordStrength] = useState(0);
   const [showPassword, setShowPassword] = useState(false);
+  const [errorVisible, setErrorVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { isDarkMode, toggleTheme } = useTheme();
 
   useEffect(() => {
     document.title = "Hamster Kombat Tool | Register";
+    
+    const content = document.querySelector(`.${styles.content}`);
+    content.style.opacity = '0';
+    content.style.transform = 'translateY(20px)';
+    
+    setTimeout(() => {
+      content.style.transition = 'opacity 1s ease, transform 2s ease';
+      content.style.opacity = '1';
+      content.style.transform = 'translateY(0)';
+    }, 100);
   }, []);
+
+  useEffect(() => {
+    if (error) {
+      setErrorVisible(true);
+      const timer = setTimeout(() => {
+        setErrorVisible(false);
+        setError('');
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
+  const clearInputs = () => {
+    setUsername('');
+    setPassword('');
+    setEmail('');
+    setPasswordStrength(0);
+  };
 
   const handleRegister = async (e) => {
     e.preventDefault();
     setError('');
     setSuccess('');
+    setLoading(true); // İşlem başladığında loading'i true yapıyoruz
   
     if (passwordStrength < 2) {
       setError('Password is too weak. Please choose a stronger password.');
+      setLoading(false); // Hata durumunda loading'i false yapıyoruz
+      setPassword('');
       return;
     }
   
@@ -37,9 +73,17 @@ function Register() {
       }, 2000);
     } catch (error) {
       setError(error.response?.data?.message || 'An error occurred during registration');
-    }
+      clearInputs();
+      console.error('Registration error', error);
+    } finally {
+      setLoading(false);
+    }    
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
@@ -54,68 +98,91 @@ function Register() {
       case 2: return 'orange';
       case 3: return 'yellow';
       case 4: return 'green';
-      default: return 'red';
+      default: return 'white';
     }
   };
 
-  const getPasswordStrengthLabel = () => {
-    switch (passwordStrength) {
-      case 0: return 'Very Weak';
-      case 1: return 'Weak';
-      case 2: return 'Average';
-      case 3: return 'Strong';
-      case 4: return 'Very Strong';
-      default: return 'Very Weak';
-    }
-  };
+
 
   return (
-    <div className="register-container">
-      <h2>Register</h2>
-      <form onSubmit={handleRegister}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <div className="password-input-container">
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          <button 
-            type="button" 
-            className="toggle-password"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? <FaEyeSlash /> : <FaEye />}
+    <div className={`${styles['login-page']} ${isDarkMode ? styles.dark : ''}`}>
+      <div className={styles.navbar}>
+        <Link to="/" className={styles['logo-container']}>
+          <img src={logoImage} alt="Logo" className={styles.logo}/>
+          <span className={styles['logo-text']}>Hamster Kombat Tool</span>
+        </Link>
+        <div className={styles['header-links']}>
+          <button onClick={toggleTheme} className={styles['theme-toggle']}>
+            {isDarkMode ? <FaSun /> : <FaMoon />}
+          </button>          
+          <button onClick={() => navigate('/login')} className={styles['link-button']}>
+            <FaSignInAlt className={styles['login-icon']} />
+            Login
           </button>
         </div>
-        <div className="password-strength">
-          <div
-            className="password-strength-bar"
-            style={{ width: `${(passwordStrength + 1) * 20}%`, backgroundColor: getPasswordStrengthColor() }}
-          ></div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles['login-container']}>
+          <h2>Register</h2>
+          <form onSubmit={handleRegister}>
+            <input
+              type="text"
+              placeholder="Username"
+              minLength="3"
+              maxLength="12"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <div className={styles['password-input-container']}>
+              <input
+                type={showPassword ? "text" : "password"}
+                minLength="6"
+                maxLength="18"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+              <button 
+                type="button" 
+                onClick={togglePasswordVisibility} 
+                className={styles['toggle-password']}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            <div className={styles['password-strength']}>
+              <div
+                className={styles['password-strength-bar']}
+                style={{ width: `${(passwordStrength + 1) * 20}%`, backgroundColor: getPasswordStrengthColor() }}
+              ></div>
+            </div>            
+            <button type="submit" disabled={loading} className={styles['register-button']}>
+  {!loading && <FaUserPlus className={styles['register-icon']} />}
+  {loading ? <div className={styles.spinner}></div> : 'Register'}
+</button>
+            <Link to="/login" className={styles['register-text']}>Already have an account?</Link>
+            {error && (
+              <p className={`${styles['error-message']} ${errorVisible ? styles.visible : ''}`}>
+                {error}
+              </p>
+            )}
+{success && (
+  <p className={`${styles['success-message']} ${success ? styles.visible : ''}`}>
+    {success}
+              </p>
+            )}
+          </form>
         </div>
-        <p className="password-strength-label">
-          Password Strength: <span style={{ color: getPasswordStrengthColor() }}>{getPasswordStrengthLabel()}</span>
-        </p>
-        <button type="submit">Register</button>
-        {success && <p className="success-message">{success}</p>}
-        {error && <p className="error-message">{error}</p>}
-      </form>
+      </div>
     </div>
   );
 }
